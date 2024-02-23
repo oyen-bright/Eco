f5d8b27c63c21f2989e3a3918dea08a7bde8e1d9
oyen-bright <oyenbrihight@gmail.com>
2024-02-23 16:31:52 +0100

    removed exposed google api key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBhSqhaS9kcx-LiEwf1Gqw_95ghaR-zXeg")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
